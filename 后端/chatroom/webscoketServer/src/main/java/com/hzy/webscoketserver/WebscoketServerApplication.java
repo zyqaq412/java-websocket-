0f4df57389caae0a83df998f16d5a2d4642ea8b0
zxwyhzy <2791517764@qq.com>
2023-05-16 12:22:30 +0800

    first commit 项目初始化

---
+++ 后端/chatroom/webscoketServer/src/main/java/com/hzy/webscoketserver/WebscoketServerApplication.java
@@ -0,0 +1,13 @@
+package com.hzy.webscoketserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebscoketServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebscoketServerApplication.class, args);
+    }
+
+}
---
+++ 后端/chatroom/webscoketServer/src/test/java/com/hzy/webscoketserver/WebscoketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hzy.webscoketserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebscoketServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
